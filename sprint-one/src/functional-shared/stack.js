7b571444782096851e32d57d53af1264cd167e1a
n-white <neil.r.whitehead@gmail.com>
2016-06-08 15:15:44 -0700

    "functional shared pattern: stack.js (initial logic added)"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  debugger;
+  var someInstance = {};
+
+  var storage = {};
+  var key = 0;
+
+  someInstance.push = function(value){
+    storage[key] = value;
+    key++;
+    console.log(storage)
+  }
+
+  someInstance.pop = function() {
+    var temp = storage[key];
+    delete storage[key];
+    key = Math.max(key--, 0);
+    console.log(temp)
+    return temp;
+  }
+
+  someInstance.size = function() {
+    return key;
+  }
+
+  return someInstance;
+
 };
 
 var stackMethods = {};
