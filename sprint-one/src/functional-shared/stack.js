9202e1bb680a54f19096d27c15a8374b60a2e81b
n-white <neil.r.whitehead@gmail.com>
2016-06-08 16:13:31 -0700

    "function shared pattern: re-factored methods under 'stackMethods'"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,3 +1,27 @@
+var stackMethods = {
+
+  push: function(value){
+    storage[key] = value;
+    key++
+    console.log("push: " + storage)
+  },
+
+  pop: function() {
+    var temp = storage[key-1];
+    delete storage[key];
+    console.log("key: " + key + "pop: " + temp)
+    key = Math.max(key-1, 0);
+    console.log("after popping, key is now at: " + key)
+    return temp;  
+  },
+
+  size: function() {
+    return key
+  }
+
+
+};
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
@@ -7,29 +31,14 @@ var Stack = function() {
   var storage = {};
   var key = 0;
 
-  someInstance.push = function(value){
-    storage[key] = value;
-    key++;
-    console.log("push: " + storage)
-  }
+  _.extend(someInstance, stackMethods)  
 
-  someInstance.pop = function() {
-    var temp = storage[key-1];
-    delete storage[key];
-    console.log("key: " + key + "pop: " + temp)
-    key = Math.max(key-1, 0);
-    console.log("after popping, key is now at: " + key)
-    return temp;  
-  }
-
-  someInstance.size = function() {
-    return key
-  }
+  console.log(someInstance)
 
   return someInstance;
 
 };
 
-var stackMethods = {};
+
 
 
