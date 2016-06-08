9d0becc147ef96e42352fb06db1502acab5e495a
n-white <neil.r.whitehead@gmail.com>
2016-06-08 16:37:13 -0700

    "function shared pattern: stack solved"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,22 +1,22 @@
 var stackMethods = {
 
   push: function(value){
-    storage[key] = value;
-    key++
-    console.log("push: " + storage)
+    this.storage[this.key] = value;
+    this.key++
+    console.log("push: " + this.storage)
   },
 
   pop: function() {
-    var temp = storage[key-1];
-    delete storage[key];
-    console.log("key: " + key + "pop: " + temp)
-    key = Math.max(key-1, 0);
-    console.log("after popping, key is now at: " + key)
+    var temp = this.storage[this.key-1];
+    delete this.storage[this.key];
+    console.log("key: " + this.key + "pop: " + temp)
+    this.key = Math.max(this.key-1, 0);
+    console.log("after popping, key is now at: " + this.key)
     return temp;  
   },
 
   size: function() {
-    return key
+    return this.key
   }
 
 
@@ -26,10 +26,10 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
-  var someInstance = {};
-
-  var storage = {};
-  var key = 0;
+  var someInstance = {
+    storage: {},
+    key: 0,
+  };
 
   _.extend(someInstance, stackMethods)  
 
