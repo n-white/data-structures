89cfe2275cb4d6add36fce2834be010b2349165b
n-white <neil.r.whitehead@gmail.com>
2016-06-08 15:02:58 -0700

    "functional pattern: queue solved"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,30 +3,67 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var key = 0
+  var key = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[key] = value
-    key++
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
-    var temp = storage[0]
-    delete storage[0]
-    key = 0
-    var newStorage = {}
+    var temp = storage[0];
+    delete storage[0];
+    key = 0;
+    var newStorage = {};
+
     for(var number in storage) {
-      newStorage[key] = storage[number]
-      key++
+      newStorage[key] = storage[number];
+      key++;
     }
-    storage = newStorage
-    return temp
+
+    storage = newStorage;
+    return temp;
+
   };
 
   someInstance.size = function() {
-    return key
+    return key;
   };
 
   return someInstance;
 };
+
+
+// var Queue = function() {
+//   var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var key = 0
+//   // Implement the methods below
+
+//   someInstance.enqueue = function(value) {
+//     storage[key] = value
+//     key++
+//   };
+
+//   someInstance.dequeue = function() {
+//     var temp = storage[0]
+//     delete storage[0]
+//     key = 0
+//     var newStorage = {}
+//     for(var number in storage) {
+//       newStorage[key] = storage[number]
+//       key++
+//     }
+//     storage = newStorage
+//     return temp
+//   };
+
+//   someInstance.size = function() {
+//     return key
+//   };
+
+//   return someInstance;
+// };
