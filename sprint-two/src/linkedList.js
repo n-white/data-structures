c578af41e15f9c5e7ebd56f23b599954f041f21b
n-white <neil.r.whitehead@gmail.com>
2016-06-09 11:33:52 -0700

    "linkedList: 1-5 test cases passing"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,54 @@
 var LinkedList = function() {
-  var list = {};
+  
+  var list = {
+    
+    // list.head = node.value
+    // list.tail = node.next.next.value
+    // list.length = 2
+
+    // list.nodes: {
+    //   node.value = 4,
+    //   node.next = {
+    //                 node.value = 5,
+    //                 node.next = {
+    //                               node.value = 6,
+    //                               node.next = null
+    //                               }
+    //               }
+
+  };
+
   list.head = null;
   list.tail = null;
+  list.length = 0;
+  list.nodes = {};
 
   list.addToTail = function(value) {
+    // if(list.head == null) {
+    //   list.head = list.tail;
+    //   // list.head.node.next = 'list.tail';
+    // }
+    // list.tail = Node(value);
+    if(list.head === null) {
+      list.tail = Node(value);
+      list.head = Node(value);
+      list.nodes = Node(value);
+      list.length++;
+    } else if (list.length > 0) {
+      list.head = list.tail;
+      list.tail = Node(value);
+      list.length++;
+    }
+
   };
 
   list.removeHead = function() {
+    if(list.length > 0) {
+      var temp = list.head;
+      delete list.head;
+      list.head = list.tail;
+      return temp.value;
+    }
   };
 
   list.contains = function(target) {
