eacc6e15df3c7309e46821e7ca6cf16e17866baa
n-white <neil.r.whitehead@gmail.com>
2016-06-08 21:25:20 -0700

    "pseudoclassical: initial stack logic added"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,54 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {}
+  this.key = 0
+
 };
 
+var Stack.prototype = {
+
+  push: function(value){
+    this.storage[this.key] = value;
+    this.key++;
+    console.log("push: " + this.storage);
+  },
+
+  pop: function() {
+    var temp = this.storage[this.key-1];
+    delete this.storage[this.key];
+    console.log("key: " + this.key + "pop: " + temp);
+    this.key = Math.max(this.key-1, 0);
+    console.log("after popping, key is now at: " + this.key);
+    return temp;  
+  },
+
+  size: function() {
+    return this.key;
+  }
+
+};
+
+
+
+
+// Stack.prototype.push = function(value){
+//     this.storage[this.key] = value;
+//     this.key++;
+//     console.log("push: " + this.storage);
+//   }
+
+// Stack.prototype.pop = function() {
+//     var temp = this.storage[this.key-1];
+//     delete this.storage[this.key];
+//     console.log("key: " + this.key + "pop: " + temp);
+//     this.key = Math.max(this.key-1, 0);
+//     console.log("after popping, key is now at: " + this.key);
+//     return temp;  
+//   }
+
+// Stack.prototype.size: function() {
+//     return this.key;
+//   }
 
