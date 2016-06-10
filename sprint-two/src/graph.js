fb4b4419164de6a758abcce12272bfe8f8de0a2d
n-white <neil.r.whitehead@gmail.com>
2016-06-09 21:59:09 -0700

    "graph: all tests solved"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -68,6 +68,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(var i = 0; i < this.nodeList.length; i++) {
+    cb(this.nodeList[i].value);
+  }
 };
 
 /*
