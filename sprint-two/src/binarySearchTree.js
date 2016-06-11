28b522b9f96003a37d3550f49f203398869643fa
n-white <neil.r.whitehead@gmail.com>
2016-06-11 15:22:52 -0700

    "binarySearchTree: solved using recursion"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,67 +12,31 @@ var binaryTreeMethods = {
 
   insert: function(number) {
 
-    var biTree = BinarySearchTree(number);
-    var currentNode = this;
-
-    while (currentNode.value !== biTree.value) {
-      // check if value of new node is greater than value of current node
-      if (biTree.value > currentNode.value) {
-      // if greater, check if value of right node of current node is null
-        if (currentNode.right === null) {
-        // if null, assign value of new node to right property
-          currentNode.right = biTree;
-        } else {
-        // if not null, reassign value of current node to equal right node (while loop will repeat) 
-          currentNode = currentNode.right;          
-        }
-
+    if (number < this.value) {
+      if (this.left === null) {
+        this.left = BinarySearchTree(number);
       } else {
-      // assume value of new node is less than value of current node
-      // if less than, check if value of left node is null
-        if (currentNode.left === null) {
-        // if null, assign value of new node to the left property
-          currentNode.left = biTree;          
-        } else {
-        // if not null, reassign value of current node to equal left node (while loop will repeat)
-          currentNode = currentNode.left;          
-        }
+        this.left.insert(number);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = BinarySearchTree(number);
+      } else {
+        this.right.insert(number);
       }
     }
   },
 
   contains: function(number) {
-    var found = false;
-    var currentNode = this;
 
-    while (currentNode !== null && found === false) {
-      // check if value of current node is equal to number
-      if (currentNode.value === number) {
-        // if equal, change value of found to be true
-        found = true;
-      // check if value of number is greater than value of current node
-      } else if (number > currentNode.value) {
-        // if greater, check if value of right node of current node is null
-        if (currentNode.right === null) {
-          // if null, reassign current node to equal null
-          currentNode = null;
-        } else {
-          // if not null, reassign value of current node to equal value of right property        
-          currentNode = currentNode.right;
-        }
-      // if its not greater, we assume it's less than with an else statement
-      } else {
-        // check if value of left node of current node is null
-          if (currentNode.left === null) {
-            // if null, reassign current node to equal null
-            currentNode = null;
-          } else {
-            // if not null, reassign value of current node to equal value of left property
-            currentNode = currentNode.left;
-          }
-      }
+    if (this.value === number) {
+      return true;
+    } else if (number > this.value) {
+      return !!(this.right && this.right.contains(number));
+    } else if (number < this.value) {
+      return !!(this.right && this.left.contains(number));
     }
-    return found;
+
   },
 
   depthFirstLog: function(callback) {
